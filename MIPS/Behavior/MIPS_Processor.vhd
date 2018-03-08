b7a3f41f6d134527d80a6b230fa8672eaa1a95a6
Quatih <hvatig@gmail.com>
2018-03-08 14:32:33 +0100

    talking points

--- MIPS/Behavior/MIPS_Processor.vhd
+++ MIPS/Behavior/MIPS_Processor.vhd
@@ -17,19 +17,10 @@ entity MIPS_Processor IS
 end MIPS_Processor;
 
 architecture behavior of MIPS_Processor is
-component memory IS
-  PORT(d_busout : OUT std_logic_vector(31 DOWNTO 0);
-       d_busin  : IN  std_logic_vector(31 DOWNTO 0);
-       a_bus    : IN  std_logic_vector(31 DOWNTO 0);
-       clk      : IN  std_ulogic;
-       write    : IN  std_ulogic;
-       read     : IN  std_ulogic;
-       ready    : OUT std_ulogic
-       );
-END component;
     signal bus_out_i, memory_location_i : std_logic_vector(word_length-1 downto 0);
     signal read_i, write_i: std_ulogic;
-    signal pc : unsigned range 0 to (2^word_length)/4; --div4 since the pc points to bytes in memory  
+    variable pc : natural;
+
 begin
     process (clk, reset)
 
@@ -38,7 +29,8 @@ begin
             read_i <= '0';
             write_i <= '0';
             bus_out_i <= (others => '0');
-            memory_location_i <= std_logic_vector(text_base_address); 
+            memory_location_i <= (others => '0');
+            pc := text_base_address; -- starting address to base address
         elsif rising_edge(clk) then
             -- read from address
             -- decode instruction
@@ -46,6 +38,7 @@ begin
             -- execute instruction
             -- store results from ALU
             -- increment program counter
+            pc := pc + text_base_size;
         end if;
     end seq;
 

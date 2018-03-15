b68ba3a3cd053772013ed9af594dffe7c5a3d923
Quatih <hvatig@gmail.com>
2018-03-16 00:04:22 +0100

    FIxed some syntax

--- MIPS/Behavior/MIPS_Processor.vhd
+++ MIPS/Behavior/MIPS_Processor.vhd
@@ -1,7 +1,6 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
-
 entity MIPS_Processor IS
   generic (word_length : integer := 32 );
   port (clk : in std_logic;
@@ -15,8 +14,11 @@ entity MIPS_Processor IS
         );
 end MIPS_Processor;
 
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
 package processor_types is
-  subtype word is std_logic_vector(word_length -1 downto 0);
+  subtype word is std_logic_vector(31 downto 0);
   subtype op_code is std_logic_vector (5 downto 0);
   subtype reg_code is std_logic_vector (4 downto 0);
   constant lw   : op_code := "100011";
@@ -54,188 +56,186 @@ USE ieee.numeric_std.ALL;
 USE work.processor_types.ALL;
 
 architecture behaviour of MIPS_Processor is
-  signal bus_out_i, memory_location_i : word;
-  signal read_i, write_i: std_ulogic;
-  variable pc : natural;
-  variable a0 : word;
-  variable a1 : word;
-  variable d0 : word;
-  variable d1 : word;
-  variable muldiv : std_logic_vector(word_length*2 -1 downto 0);
-    alias lo : word is muldiv(word_length*2 -1 downto word_length -1);
-    alias hi : word is muldiv(word_length -1 downto 0);
-  variable data : integer; -- temp variable
-  variable datareg : word; -- temp variable
-  variable cc : std_logic_vector (2 downto 0); -- clear condition code register;
-    alias cc_n  : std_logic IS cc(2); -- negative
-    alias cc_z  : std_logic IS cc(1); -- zero
-    alias cc_v  : std_logic IS cc(0); -- overflow/compare
-  variable current_instr: word;
-    alias opcode : op_code IS current_instr(31 downto 26);
-    alias rs : reg_code IS current_instr(25 downto 21);
-    alias rt : reg_code IS current_instr(20 downto 16);
-    alias imm : reg_code IS current_instr(15 downto 0);
-    alias rd : reg_code Is current_instr(15 downto 11);
-    alias rtype : op_code IS current_instr(5 downto 0);
-
-  procedure set_cc_rd (data : in integer
-                      cc : out std_logic_vector(2 downto 0)
-                      regval : out word) is
-    constant low  : integer := -2**(word_length - 1);
-    constant high : integer := 2**(word_length - 1) - 1;
-    begin
-      if (to_signed(data)<low) or (to_signed(data)>high)
-      then -- overflow
-        ASSERT false REPORT "overflow situation in arithmetic operation" SEVERITY 
-        note;
-        cc_v:='1'; cc_n:='-'; cc_z:='-';
-        regval := (others => '-');
-      else
-        cc_v:='0'; 
-        if(data <0) then
-            cc_n:='1';
-        else
-            cc_n = '0'
-        end if; 
-        if(data = 0) then
-            cc_z = '1';
-        else
-            cc_z = '0';       
-        end if;
-        regval := std_logic_vector(to_unsigned(data, word_length));
-      end if;
-  end set_cc_rd;
-
-  procedure memory_read (addr   : in natural;
-                          result : out word) IS
-  -- Used 'global' signals are:
-  --   clk, reset, ready, read, a_bus, d_busin
-  -- read data from addr in memory
   begin
-    -- put address on output
-    memory_location_i <= std_logic_vector(to_unsigned(addr,word_length));
-    wait until clk='1';
-    if reset='1' then
-      return;
-    end if;
-
-    loop -- ready must be low (handshake)
-      if reset='1' then
-        return;
-      end if;
-      exit when ready='0';
-      wait until clk='1';
-    end loop;
-
-    read_i <= '1';
-    wait until clk='1';
-    if reset='1' then
-      return;
-    end if;
-
-    loop
-      wait until clk='1';
-      if reset='1' then
-        return;
-      end if;
-
-      if ready='1' then
-        result := bus_in;
-        EXIT;
-      end if;    
-    end loop;
-    wait until clk='1';
-    if reset='1' then
-      return;
-    end if;
-
-    read_i <= '0'; 
-    memory_location_i <= (others => '0';
-  end memory_read;                         
-
-  procedure memory_write(addr : in natural;
-                          data : in word) IS
-  -- Used 'global' signals are:
-  --   clk, reset, ready, write, a_bus, d_busout
-  -- write data to addr in memory
-    VARIABLE add : bit16;
-  begin
-    -- put address on output
-    memory_location_i <= std_logic_vector(to_unsigned(addr,word_length));
-    wait until clk='1';
-    if reset='1' then
-      return;
-    end if;
-
-    loop -- ready must be low (handshake)
-      if reset='1' then
-        return;
-      end if;
-      exit when ready='0';
-      wait until clk='1';
-    end loop;
-
-    bus_out_i <= data;
-    wait until clk='1';
-    if reset='1' then
-      return;
-    end if;  
-    write_i <= '1';
-
-    loop
-      wait until clk='1';
-      if reset='1' then
-        return;
-      end if;
-        exit when ready='1';  
-    end loop;
-    wait until clk='1';
-    if reset='1' then
-      return;
-    end if;
-    --
-    write_i <= '0';
-    bus_out_i <= (others => '0');
-    memory_location_i <= (others => '0');
-  end memory_write;
-
-  function read_data(source : in reg_code ) return integer is;
-  variable ret : integer;
-  begin
-    case source is
-      when none => ret := 0;
-      when instr_imm => ret := to_integer(imm);
-      when reg_d0 => ret := d0;
-      when reg_d1 => ret := d1;
-      when reg_a0 => ret := a0;
-      when reg_a1 => ret := a1;
-      when a0_addr => memory_read(a0, ret);
-      when a1_addr => memory_read(a1, ret);
-      when others => assert false report "illegal source when reading data" severity warning;
-    end case;
-    return ret;
-  end read_data;
-
-  procedure write_data(destination : in reg_code;
-                        d0, d1, a0, a1 : inout word;
-                        data : in word)is
-  begin
-    case destination is
-      when none => NULL;
-      when instr_imm => NULL;
-      when reg_d0 => d0 := data;
-      when reg_d1 => d1 := data;
-      when reg_a0 => a0 := data;
-      when reg_a1 => a1 := data;
-      when a0_addr => memory_write(a0, data);
-      when a1_addr => memory_write(a1, data);
-        when others => assert false report "illegal source when reading data" severity warning;
-    end case;
-  end write_data;
-
-begin
-  process
+    process
+      signal bus_out_i, memory_location_i : word;
+      signal read_i, write_i: std_ulogic;
+      variable pc : natural;
+      variable a0 : word;
+      variable a1 : word;
+      variable d0 : word;
+      variable d1 : word;
+      variable muldiv : std_logic_vector(word_length*2 -1 downto 0);
+        alias lo : word is muldiv(word_length*2 -1 downto word_length -1);
+        alias hi : word is muldiv(word_length -1 downto 0);
+      variable data : integer; -- temp variable
+      variable datareg : word; -- temp variable
+      variable cc : std_logic_vector (2 downto 0); -- clear condition code register;
+        alias cc_n  : std_logic IS cc(2); -- negative
+        alias cc_z  : std_logic IS cc(1); -- zero
+        alias cc_v  : std_logic IS cc(0); -- overflow/compare
+      variable current_instr: word;
+        alias opcode : op_code IS current_instr(31 downto 26);
+        alias rs : reg_code IS current_instr(25 downto 21);
+        alias rt : reg_code IS current_instr(20 downto 16);
+        alias imm : reg_code IS current_instr(15 downto 0);
+        alias rd : reg_code Is current_instr(15 downto 11);
+        alias rtype : op_code IS current_instr(5 downto 0);
       variable tmp : std_logic_vector(word_length*2-1 downto 0);
+      procedure set_cc_rd (data : in integer;
+                          cc : out std_logic_vector(2 downto 0);
+                          regval : out word) is
+        constant low  : integer := -2**(word_length - 1);
+        constant high : integer := 2**(word_length - 1) - 1;
+      begin
+        if (data<low) or (data>high)
+        then -- overflow
+          ASSERT false REPORT "overflow situation in arithmetic operation" SEVERITY 
+          note;
+          cc_v:='1'; cc_n:='-'; cc_z:='-';
+          regval := (others => '-');
+        else
+          cc_v:='0'; 
+          if(data <0) then
+              cc_n:='1';
+          else
+              cc_n := '0';
+          end if; 
+          if(data = 0) then
+              cc_z := '1';
+          else
+              cc_z := '0';       
+          end if;
+          regval := std_logic_vector(to_unsigned(data, word_length));
+        end if;
+    end set_cc_rd;
+
+    procedure memory_read (addr   : in natural;
+                            result : out word) IS
+    -- Used 'global' signals are:
+    --   clk, reset, ready, read, a_bus, d_busin
+    -- read data from addr in memory
+    begin
+      -- put address on output
+      memory_location_i <= std_logic_vector(to_unsigned(addr,word_length));
+      wait until clk='1';
+      if reset='1' then
+        return;
+      end if;
+
+      loop -- ready must be low (handshake)
+        if reset='1' then
+          return;
+        end if;
+        exit when ready='0';
+        wait until clk='1';
+      end loop;
+
+      read_i <= '1';
+      wait until clk='1';
+      if reset='1' then
+        return;
+      end if;
+
+      loop
+        wait until clk='1';
+        if reset='1' then
+          return;
+        end if;
+
+        if ready='1' then
+          result := bus_in;
+          EXIT;
+        end if;    
+      end loop;
+      wait until clk='1';
+      if reset='1' then
+        return;
+      end if;
+
+      read_i <= '0'; 
+      memory_location_i <= (others => '0');
+    end memory_read;                         
+
+    procedure memory_write(addr : in natural;
+                            data : in word) IS
+    -- Used 'global' signals are:
+    --   clk, reset, ready, write, a_bus, d_busout
+    -- write data to addr in memory
+    begin
+      -- put address on output
+      memory_location_i <= std_logic_vector(to_unsigned(addr,word_length));
+      wait until clk='1';
+      if reset='1' then
+        return;
+      end if;
+
+      loop -- ready must be low (handshake)
+        if reset='1' then
+          return;
+        end if;
+        exit when ready='0';
+        wait until clk='1';
+      end loop;
+
+      bus_out_i <= data;
+      wait until clk='1';
+      if reset='1' then
+        return;
+      end if;  
+      write_i <= '1';
+
+      loop
+        wait until clk='1';
+        if reset='1' then
+          return;
+        end if;
+          exit when ready='1';  
+      end loop;
+      wait until clk='1';
+      if reset='1' then
+        return;
+      end if;
+      --
+      write_i <= '0';
+      bus_out_i <= (others => '0');
+      memory_location_i <= (others => '0');
+    end memory_write;
+
+    function read_data(source : in reg_code ) return integer is
+      variable ret : integer;
+    begin
+      case source is
+        when none => ret := 0;
+        when instr_imm => ret := to_integer(imm);
+        when reg_d0 => ret := d0;
+        when reg_d1 => ret := d1;
+        when reg_a0 => ret := a0;
+        when reg_a1 => ret := a1;
+        when a0_addr => memory_read(a0, ret);
+        when a1_addr => memory_read(a1, ret);
+        when others => assert false report "illegal source when reading data" severity warning;
+      end case;
+      return ret;
+    end read_data;
+
+    procedure write_data(destination : in reg_code;
+                          d0, d1, a0, a1 : inout word;
+                          data : in word)is
+    begin
+      case destination is
+        when none => NULL;
+        when instr_imm => NULL;
+        when reg_d0 => d0 := data;
+        when reg_d1 => d1 := data;
+        when reg_a0 => a0 := data;
+        when reg_a1 => a1 := data;
+        when a0_addr => memory_write(a0, data);
+        when a1_addr => memory_write(a1, data);
+          when others => assert false report "illegal source when reading data" severity warning;
+      end case;
+    end write_data;
+      
   begin
     if reset = '1' then
         read_i <= '0';

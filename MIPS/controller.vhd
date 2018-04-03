54de7bd6553ce0cdc25f3aa6a3c40060a656498f
Quatih <hvatig@gmail.com>
2018-04-03 16:03:00 +0200

    Implemented a lot of instructions

--- MIPS/controller.vhd
+++ MIPS/controller.vhd
@@ -7,18 +7,18 @@ USE work.memory_config.ALL;
 entity controller is
     generic (word_length : natural);
     port (
-        control  : out control_bus;
-        instr : in word;
-        alu_ops : out alu_bus;
-        bus_in : in std_logic_vector(word_length-1 downto 0);
-        bus_out : out std_logic_vector(word_length-1 downto 0);
-        memory_location : out std_logic_vector(word_length-1 downto 0);
-        clk : in std_ulogic;
-        write : out std_ulogic;
-        read : out std_ulogic;
-        ready : in std_ulogic;
-        reset : in std_ulogic
-        );
+			clk : in std_ulogic;
+			ready : in std_ulogic;
+			reset : in std_ulogic;
+			cc : in cc_type;
+				alias cc_n  : std_logic IS cc(2); -- negative
+				alias cc_z  : std_logic IS cc(1); -- zero
+				alias cc_v  : std_logic IS cc(0); -- overflow/compare
+			control  : out control_bus;
+			instr : in word;
+			alu_out : out alu_code;
+			alu_ready : in std_ulogic
+			);
 end controller;
 
 
@@ -26,12 +26,12 @@ architecture behaviour of controller is
     type register_file is array (0 to 31) 
     of word;
     signal current_instr : word;
-        alias opcode : op_code IS current_instr(31 downto 26);
-        alias rs : reg_code IS current_instr(25 downto 21);
-        alias rt : reg_code IS current_instr(20 downto 16);
-        alias rd : reg_code Is current_instr(15 downto 11);
-        alias imm : hword IS current_instr(15 downto 0);
-        alias rtype : op_code IS current_instr(5 downto 0);
+			alias opcode : op_code IS current_instr(31 downto 26);
+			alias rs : reg_code IS current_instr(25 downto 21);
+			alias rt : reg_code IS current_instr(20 downto 16);
+			alias rd : reg_code Is current_instr(15 downto 11);
+			alias imm : hword IS current_instr(15 downto 0);
+			alias rtype : op_code IS current_instr(5 downto 0);
 
 begin
     current_instr <= instr;
@@ -48,43 +48,86 @@ begin
               exit when reset = '0';
             end loop;
         elsif(rising_edge(clk)) then
-            control <= (read_mem => '1', others => '0'); 
-            loop 
-                wait until rising_edge(clk);
-                exit when ready = '1';
-            end loop;
+					control <= (read_mem => '1', others => '0'); 
+					loop 
+						wait until rising_edge(clk);
+						exit when ready = '1';
+					end loop;
+					case opcode is --decode instruction
+						when "00000"=>
+						case rtype is 
+							when nop  =>  assert false report "finished calculation" severity failure;
+							when mfhi =>  control <= (rwrite => '1', spreg => '1', lohisel =>'1', others => '0');
+							when mflo =>  control <= (rwrite => '1', spreg => '1', others => '0');
+							when mult =>  
+								control <= (alusrc => '1', rread => '1', others => '0');
+								alu_out <= alu_mult;
+								wait until alu_ready = '1';
+							when div  =>  
+								control <= (alusrc => '1', rread => '1', others => '0');
+								alu_out <= alu_div;
+								wait until alu_ready = '1'; 
+							when orop =>  
+								control <= (rread => '1', others => '0') -- move to alu inputs, 
+								alu_out <= alu_or;
+								wait until alu_ready = '1';
+								control <= (rdest => '1', rwrite => '1', others => '0'); --move from alu to rdst
+							when add  =>  
+								control <= (rread => '1', others => '0') -- move to alu inputs, 
+								alu_out <= alu_add;
+								wait until alu_ready = '1';
+								control <= (rdest => '1', rwrite => '1', others => '0'); --move from alu to rdst
 
-            case opcode is --decode instruction
-                when "00000"=>
-                case rtype is 
-                    when nop    => assert false report "finished calculation" severity failure;
-                    when mfhi   => control <= (rwrite => '1', rdest => '1', spreg => '1', lohisel =>'1', others => '0');
-                                   wait until rising_edge(clk);
-                    when mflo   => control <= (rwrite => '1', rdest => '1', spreg => '1', others => '0');
-                                   wait until rising_edge(clk);
-                    when mult   => 
-                    when div    => 
-                    when orop   => 
-                    when add    => 
-                    when subop  => 
-                    when slt    => 
-                    when others =>
-                        control <= (others => '0');
-                        assert false report "illegal r-type instruction" severity warning;
-                end case;
-                when lw     =>
-                when sw     =>
-                when beq    =>
-                when bgez   =>
-                when ori    =>
-                when addi   =>
-                when lui    =>
-                
-                when others =>
-                    control <= (others => '0'); 
-                    assert false report "illegal instruction" severity warning;
-            end case;
-            
+							when subop=>  
+								control <= (rread => '1', others => '0') -- move to alu inputs, 
+								alu_out <= alu_sub;
+								wait until alu_ready = '1';
+								control <= (rdest => '1', rwrite => '1', others => '0'); --move from alu to rdst
+
+							when slt  =>  
+								control <= (rread => '1', others => '0') -- move to alu inputs, 
+								alu_out <= alu_lt;
+								wait until alu_ready = '1';
+								control <= (rdest => '1', rwrite => '1', others => '0'); --move from alu to rdst
+
+							when others =>  
+								control <= (others => '0');
+								assert false report "illegal r-type instruction" severity warning;
+						end case;
+						when lw   => 
+							control <= (rread => '1', alusrc => '1', others => '0'); --calc addr
+							alu_out <= alu_add;
+							wait until alu_ready = '1';
+							control <= (mread => '1', msrc => '1', others => '0'); --load word
+							wait until ready = '1';
+							control <= (rwrite => '1', wregsrc => '1', others => '0'); --write word to reg
+						when sw   => 
+							control <= (rread => '1', alusrc => '1', others => '0'); --calc addr
+							alu_out <= alu_add;
+							wait until alu_ready = '1';
+							control <= (mwrite => '1', msrc => '1', others => '0'); --load word
+							wait until ready = '1';
+
+						when beq  =>
+							control <= (rread => '1', others => '0'); --calc addr
+							alu_out <= alu_sub;
+							wait until alu_ready = '1';
+							if(cc_z = '1') then
+								control <= (pcimm => '1', others => '0');
+							end if;
+						when bgez	=>
+							control <= (rread => '1', others => '0'); --calc addr
+							alu_out <= alu_gz;
+							wait until alu_ready = '1';
+							if(cc_z) 
+						when ori	=>
+						when addi =>
+						when lui  =>
+						when others =>
+							control <= (others => '0'); 
+							assert false report "illegal instruction" severity warning;
+					end case;
+					
         end if;
     end process;
 end behaviour;

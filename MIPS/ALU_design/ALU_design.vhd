5367412c4c890da8e6148a2750bdfe7d2ce30891
Quatih <hvatig@gmail.com>
2018-04-17 23:56:02 +0200

    expanded alu_design with booth algo

--- MIPS/ALU_design/ALU_design.vhd
+++ MIPS/ALU_design/ALU_design.vhd
@@ -55,8 +55,38 @@ architecture alu of alu_design is
 		end if;
 	end set_cc;
 
+	procedure mult_booth(	op1, op2 	: in std_logic_vector;
+											 	signal result : out signed(word_length*2 -1 downto 0)) is
+		variable mult1, mult2, minus_multi : signed (word_length-1 downto 0);
+		variable prod_sft_add : std_logic_vector(word_length*2 downto 0);
+		constant ub : natural := word_length*2; -- upper bound
+		constant lb : natural := word_length+1; -- lower bound
+	begin
+		mult1 := signed(op1);
+		mult2 := signed(op2);
+		prod_sft_add(ub downto lb) := (others => '0');
+		prod_sft_add(word_length downto 1) := std_logic_vector(mult1);
+		prod_sft_add(0) := '0';
+		minus_multi := signed(not op2 ) + 1;
+		for i in 0 to word_length-1 loop
+			
+			case prod_sft_add(1 downto 0) is
+			when "00"|"11" => 
+									prod_sft_add := prod_sft_add(ub) & prod_sft_add(ub downto 1);
+			when "01"      => 
+									prod_sft_add(ub downto lb) := std_logic_vector(mult1 + mult2);
+									prod_sft_add := prod_sft_add(ub) & prod_sft_add(ub downto 1);
+			when "10"      => 
+									prod_sft_add(ub downto lb) := std_logic_vector(mult1 - minus_multi);
+									prod_sft_add := prod_sft_add(ub) & prod_sft_add(ub downto 1);
+			when others => prod_sft_add := (others => '0'); 
+			end case;
+		end loop;
+		result <= signed(prod_sft_add(ub downto 1));
+	end mult_booth;
 begin
 	seq: process
+		variable lop1, lop2 : signed(word_length*2-1 downto 0) := (others => '0');
 	begin
 
 	if reset = '1' then
@@ -66,12 +96,18 @@ begin
 	end if;
 	wait until rising_edge(clk);
 	if start = '1' then
+		lop1(word_length-1 downto 0) := signed(op1);
+		lop1(word_length*2-1 downto word_length) := (others =>'0');
+		lop2(word_length-1 downto 0) := signed(op2);
+		lop2(word_length*2-1 downto word_length) := (others =>'0');
 		case inst is
-			when alu_add => 	calc <= signed(op1) + signed(op2);
+			when alu_add => calc <= lop1 + lop2;
 											set_cc(calc,cc);
-		when alu_mult => 	calc <= multiply(op1,op2);
+		when alu_mult => 	
+											--calc <= multiply(op1,op2);
+											mult_booth(op1, op2, calc);
 											set_cc(calc,cc);
-		when alu_sub 	=> 	calc <= signed(op1) - signed(op2);
+		when alu_sub 	=> 	calc <= lop1 - lop2;
 											set_cc(calc,cc);
 		when alu_div =>   calc(word_length*2-1 downto word_length) <= signed(op1) mod signed(op2);
 											calc(word_length-1 downto 0) <= signed(op1) / signed(op2);

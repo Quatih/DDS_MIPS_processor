e6692903060b3831a0fe3acc6f6d2435ac01e964
Quatih <hvatig@gmail.com>
2018-04-21 13:49:47 +0200

    some dprtl changes

--- MIPS/datapath_rtl.vhd
+++ MIPS/datapath_rtl.vhd
@@ -3,7 +3,7 @@
 architecture rtl of datapath is
   constant zero       : word := (others=>'0');
   constant dontcare   : word := (others=>'-'); 
-  
+
   type register_file is array (0 to 31) 
     of std_logic_vector(word_length-1 downto 0);
   signal regfile  : register_file;
@@ -45,18 +45,63 @@ architecture rtl of datapath is
       regfile(to_integer(unsigned(destination))) <= data;
     end if;
   end write_reg;
+      
+  function sign_extend(vec : hword) return word
+    variable ret : word;
+  begin
+    ret(31 downto 16) <= (others => vec(15)); -- sign extend imm
+    ret(15 downto 0) <= vec;
+    return ret;
+  end sign_extend;
   
+  function load_upper(vec : hword) return word
+    variable ret : word;
+  begin
+    ret(31 downto 16) <= vec; -- sign extend imm
+    ret(15 downto 0) <= (others => '0');
+    return ret;
+  end load_upper;
+
+  function seshift(vec : hword) return word --sign extend and shift
+    variable ret : word;
+  begin
+      ret(31 downto 18) := (others => vec(15)); -- sign extend
+      ret(17 downto 2) := vec;
+      ret(1 downto 0) := (others => '0');
+    return ret;
+  end seshift;
+
 begin
   control <= std2ctlr(ctrl_std);
   -- using control conversion
   ready <= ready_i;
-  alu_op1 <= op1;
-  alu_op2 <= op2;
 
+  alu_op1 <=  read_reg(rs) when control(rread) = '1' else
+              dontcare;
+  alu_op2 <=  read_reg(rt)      when control(rread) = '1' else
+              sign_extend(imm)  when control(rread) = '1' and control(alusrc) = '1' else
+              load_upper(imm)   when control(rread) = '1' and control(alusrc) = '1' and control(immsl) = '1' else 
+              dontcare; -- or alu_op2?
+  pc <= std_logic_vector(unsigned(pc) + 4) when control(pcincr) = '1' else
+        std_logic_vector(signed(pc) + signed(seshift(imm))) when control(pcimm) = '1' else
+        pc;
 
 process 
 begin
   wait until clk = '1'
+  if(rwrite = '1') then
+    if control(rspreg) = '1' then -- if write from spreg (mfhi and mflo)
+      if control(lohisel) = '1' then --hi
+        write_reg(rd, regfile, hi);
+      else --lo
+        write_reg(rd, regfile, lo);
+      end if;
+    elsif(control(rdest) = '1') then --write to rd, all rtype instr 
+      write_reg(rd, regfile, aluword);
+    else
+      write_reg(rt, regfile, aluword);
+    end if;
+  end if;
 end process;
 end rtl;
 
